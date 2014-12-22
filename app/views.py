bef6c3d388950b9e56ec07bd2f12b63f7b91f909
S1M1S <samm0s@hotmail.com>
2014-12-23 10:04:17 +1100

    Completed site structure, updated views.py, added slabtext.js, removed readme, completed base styling

--- app/views.py
+++ app/views.py
@@ -4,10 +4,30 @@ import app.models
 TEMPLATE_PATH.append('./app/templates')
 
 @route('/')
-@route('/home')
+@route('/landing')
 def home():
     return template('landing.html')
 
-@route('/website-design')
+@route('/website-design/information')
 def website_design():
-    return template('website_design/website-design.html')
+    return template('website_design/information.html')
+
+@route('/website-design/portfolio')
+def website_design():
+    return template('website_design/portfolio.html')
+
+@route('/website-design/contact-me')
+def website_design():
+    return template('website_design/contact-me.html')
+
+@route('/internet-radio/information')
+def website_design():
+    return template('internet_radio/information.html')
+
+@route('/internet-radio/listen')
+def website_design():
+    return template('internet_radio/listen.html')
+
+@route('/music-production/information')
+def website_design():
+    return template('music_production/information.html')
