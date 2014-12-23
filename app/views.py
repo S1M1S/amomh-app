4bd1e600bd4b7a00ed551617cb5de7b825a053e6
S1M1S <samm0s@hotmail.com>
2014-12-23 14:01:56 +1100

    Apparently I edited these files

--- app/views.py
+++ app/views.py
@@ -1,36 +1,43 @@
 from bottle import TEMPLATE_PATH, route, jinja2_template as template
-import app.models
 
 TEMPLATE_PATH.append('./app/templates')
+
 
 @route('/')
 @route('/landing')
 def home():
     return template('landing.html')
 
+
 @route('/website-design/information')
 def website_design():
     return template('website-design/information.html')
+
 
 @route('/website-design/portfolio')
 def website_design():
     return template('website-design/portfolio.html')
 
+
 @route('/website-design/quote-estimates')
 def website_design():
     return template('website-design/quote-estimates.html')
+
 
 @route('/website-design/contact-me')
 def website_design():
     return template('website-design/contact-me.html')
 
+
 @route('/internet-radio/information')
 def website_design():
     return template('internet-radio/information.html')
 
+
 @route('/internet-radio/listen')
 def website_design():
     return template('internet-radio/listen.html')
+
 
 @route('/music-production/information')
 def website_design():
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ package_dir_path = os.path.join(os.path.dirname(__file__), package_dir)
 sys.path.insert(0, package_dir_path)
 
 import bottle
-from app import views
 
 bottle.debug(True)
 bottle.run(server='gae')
